31374fee8354f31b4009165c47f5d6d931136e56
LTMM51 <luthiagomm@gmail.com>
2023-10-04 15:13:12 -0300

    corrigi 2: o bug agora é outro

--- conta_ponto.py
+++ conta_ponto.py
@@ -2,9 +2,9 @@ from equipes import equipes
 
 dic1 = {}
 
-relatorio = 100
-bateria_atual = 1
-CPreal = 10
+relatorio = 185
+bateria_atual = 2
+CPreal = 15
 CPprevisto = 9.1
 PVreal = 5.5
 PVprevisto = 5.3
@@ -34,20 +34,26 @@ Pac = max(0, 30 - 830 * (abs((CPprevisto-CPreal)/CPprevisto))**1.75)
 
 Brc = (min(20, 40-t/3) if t<120 else 0)
 
-rodadas_nois = []
+pontos = Brc + Pac + Pvoo  + CPreal
+
+rodadas_nois = [pontos]
 
 if bateria_atual >1:
-    for bate in range(1,bateria_atual+1):
+    for bate in range(1,bateria_atual):
         rodadas_nois.append(equipes[34][bate])
         rodadas_nois.sort()
 try:
     BCFnosso = 20*(1-(5*(rodadas_nois[-1]-rodadas_nois[-2])/rodadas_nois[-1])**2)
+    BCFnosso = (BCFnosso if BCFnosso>0 else 0)
+    print(rodadas_nois[-1])
+    print(rodadas_nois[-2])
+    print(BCFnosso)
 except:
     BCFnosso = 0
 
-pontos = Brc + Pac + Pvoo + BCFnosso + CPreal
+pontos+=BCFnosso
 
-dic1[34] = sum(rodadas_nois) + pontos - equipes[34]["penalidade"] + equipes[34]["bonificacao"]
+dic1[34] = sum(rodadas_nois) - equipes[34]["penalidade"] + equipes[34]["bonificacao"]
 
 podio = dict(reversed(sorted(dic1.items(), key=lambda item: item[1])))
 
@@ -57,4 +63,5 @@ for time in podio.keys():
     times.append(time)
 
 print(f'Posição no podio com CP de {CPreal} é de: {times.index(34)+1}')
+print(podio[34])
 
